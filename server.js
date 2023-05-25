b33ff10c22f66da6ad611b96bce591e51b5f7d27
Grant Holley <97457524+gh0lree21@users.noreply.github.com>
2023-05-25 10:59:11 -0600

    Added /grant endpoint

--- server.js
+++ server.js
@@ -10,4 +10,8 @@ app.get('/landon', (req,res) => {res.send("Hello Landon")});
 
 app.get('/samantha', (req,res) => {res.send("Hello Samantha")});
 
+app.get('/grant', (req, res)=> {
+    res.send("Hello Grant");
+})
+
 app.listen(port, ()=>console.log("Listening"));
