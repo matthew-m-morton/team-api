4d681f1e6d44db90b50abf2f34b22fc3bddd1d23
BPayne22 <bpayne22@byui.edu>
2023-06-01 09:58:37 -0600

    Fixed issue with babel/core

---
+++ test/brian.test.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch2')
+
+it ("Should test validity of token", async()=>{
+    let token = "";
+
+    const options = {
+        method: 'POST',
+        headers: {
+            "content-type":"application/json"
+
+        },
+
+    };
+    const response = await fetch('http://localhost:3000/brian')
+    //token = await response.text()
+    const name = await response.text();
+    expect(name).toBe("Hello Brian")
+
+});
